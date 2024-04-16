2f147b4f9af29c3419424044bd867d10c84ba263
Artem Byastic <stalker3343@gmail.com>
2024-04-16 15:58:12 +0300

    update shepes color and cursor-div-vrapper z-index

--- components/ShapesBg copy.vue
+++ components/ShapesBg copy.vue
@@ -164,27 +164,28 @@ onMounted(() => {
 
 <template>
   <div class="color-dodge-wrapper">
-
     <div
       id="realFollowCursor"
       class="cursor-div-vrapper"
     >
       <CorsorElement class="corsor-elem"></CorsorElement>
+      <!-- <img
+        class="corsor-elem"
+        src="/bg/cursor.svg"
+      > -->
       <!-- <svg
         class="corsor-elem"
-        width="930"
-        height="880"
         viewBox="0 0 930 880"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <g
-          style="mix-blend-mode:saturation"
+          style="mix-blend-mode:soft-light"
           filter="url(#filter0_f_1229_6999)"
         >
           <path
             d="M259.051 625.088C311.258 716.957 357.743 666.229 374.461 629.382C397.012 552.45 458.688 428.056 524.979 545.936C591.27 663.816 660.882 631.306 687.401 600.316C734.437 550.133 778.41 444.363 578.01 422.749C377.61 401.135 347.125 360.253 330.371 309.212C320.952 280.518 331.211 136.036 261.195 241.116C173.676 372.465 193.793 510.253 259.051 625.088Z"
-            fill="#33FF00"
+            fill="#FFE0E0"
           />
         </g>
         <defs>
@@ -208,25 +209,65 @@ onMounted(() => {
               result="shape"
             />
             <feGaussianBlur
-              stdDeviation="100.32"
+              stdDeviation="70.32"
               result="effect1_foregroundBlur_1229_6999"
             />
           </filter>
         </defs>
       </svg> -->
-      <!--     <canvas></canvas> -->
+
+
+      <!-- <svg
+        class="corsor-elem"
+        viewBox="0 0 620 587"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g style="mix-blend-mode:soft-light">
+          <path
+            d="M172.615 416.859C207.396 478.062 238.365 444.267 249.502 419.719C264.526 368.467 305.614 285.595 349.778 364.127C393.941 442.66 440.317 421.001 457.984 400.356C489.32 366.923 518.614 296.459 385.107 282.06C251.6 267.661 231.291 240.424 220.129 206.421C213.854 187.305 220.689 91.0502 174.044 161.055C115.739 248.56 129.14 340.355 172.615 416.859Z"
+            fill="#FFE0E0"
+          />
+        </g>
+
+      </svg> -->
     </div>
+    <!-- <img
+      class="shapes-all-noth"
+      src="/bg/Northern.svg"
+    > -->
     <ShepesElement class="shapes-all"></ShepesElement>
+    <!-- <img
+      class="shapes-all"
+      src="/bg/shapes-new.svg"
+    > -->
+
+    <!-- <img
+      class="blob-6"
+      src="/bg/blob6.svg"
+    >
+    <img
+      class="blob-5"
+      src="/bg/blob5.svg"
+    >
+
+    <img
+      class="blob-4"
+      src="/bg/blob4.svg"
+    >
+
+    <img
+      class="blob-3"
+      src="/bg/blob3.svg"
+    >
 
 
+    <img
+      class="blob-1"
+      src="/bg/blob1.svg"
+    > -->
 
-    <!-- <ShepesElement class="shapes-all"></ShepesElement> -->
 
-
-
-
-    <!--   <canvas class="shapes-all" id="canvas2"></canvas>
--->
   </div>
 
 </template>
@@ -237,20 +278,40 @@ onMounted(() => {
   position: absolute;
   top: 0px;
   left: 0px;
-  // height: 100%;
   z-index: 1;
 
-}
 
+
+
+  // g {
+
+  //   // background: #BD00FF;
+  //   // mix-blend-mode: hard-light;
+  //   filter: blur(101.32px);
+  // }
+
+
+
+}
+</style>
+
+<style scoped lang="scss">
 .cursor-div-vrapper {
   position: fixed;
   width: calc(556.15px * 2.2);
-  height: calc(395.86px * 2.2);
+  height: calc(395.86px* 2.2);
   pointer-events: none;
-  z-index: 1;
-  /*   transform: translate3d(0, 0, 0);
- */
+  z-index: 2;
   /* border: 24px solid white; */
+}
+
+
+.shapes-all-noth {
+  width: 100%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 2;
 }
 
 .color-dodge-wrapper {
@@ -262,30 +323,154 @@ onMounted(() => {
   left: 0px;
   width: 100%;
   height: 100%;
-  /*     mix-blend-mode: color-dodge;
-
-
-  mix-blend-mode: lighten; !!!!
-   mix-blend-mode: difference;
-  mix-blend-mode: hue; 
-    mix-blend-mode: plus-darker;
- 
-  
-  */
-
   mix-blend-mode: lighten;
-  /*   transform: translate3d(0, 0, 0); */
 }
 
-.corsor-elem {
-  pointer-events: none;
-  // transform: translate3d(0, 0, 0);
+.blob-6 {
+  /* blob6 */
 
-  /*   filter: blur(101.32px); */
+  position: absolute;
+  width: calc(358.93px* 1.9);
+
+  height: calc(285.9px*1.9);
+
+  left: 562px;
+  top: 100px;
+
+  /* background: #BD00FF;
+  mix-blend-mode: hard-light;
+  filter: blur(101.32px); */
+
+
+}
+
+.blob-5 {
+  position: absolute;
+
+
+  left: 330px;
+  top: 290px;
+
+  width: calc(616.93px* 1.9);
+  height: calc(559.89px*1.9);
+
+
+
+
+
+}
+
+.blob-4 {
+  position: absolute;
+  left: 970px;
+  top: 590px;
+
+  width: calc(520px* 1.7);
+  height: calc(414px*1.7);
+
+}
+
+
+.blob-3 {
+  position: absolute;
+  left: 970px;
+  top: 0px;
+
+  width: calc(890px* 1.9);
+  height: calc(811px*1.9);
+
+}
+
+.blob-1 {
+  position: absolute;
+  left: -100px;
+  top: 600px;
+
+  width: calc(740px* 1.5);
+  height: calc(670px*1.5);
+
+}
+
+
+
+
+
+
+.corsor-elem {
+  /* cursor */
+  pointer-events: none;
+
   position: absolute;
   top: 0px;
   left: 0px;
   width: 100%;
   height: 100%;
+
+  /* cursor */
+
+
+  /* transform: rotate(-146.6deg); */
+
+
+  /* cursor */
+
+  position: absolute;
+
+
+
+  // background: #FFE0E0;
+  // mix-blend-mode: soft-light;
+
+  // filter: blur(101.32px);
+
+
+  // transform: rotate(-146.6deg);
+
+
+
+
+  /* left: 791.3px;
+  top: 810.64px; */
+
+  /* background: #FFE0E0; */
+  /* filter: blur(101.32px); */
+
+
+
+
+  /* background: #FFE0E0; */
+  /* mix-blend-mode: soft-light; */
+  /* blur 1 */
+  /* filter: blur(0px); */
+  /* transform: matrix(-0.88, -0.48, 0.62, -0.78, 0, 0); */
+
+
+
+
+  /* background: #FFE0E0; */
+  /* mix-blend-mode: soft-light; */
+
+}
+
+
+
+.blob-2 {
+  /* blob2 */
+
+  position: absolute;
+  width: 674px;
+  height: 384px;
+  left: 144.97px;
+  top: 0px;
+
+  /* background: #BD00FF; */
+  /* mix-blend-mode: hard-light; */
+
+  mix-blend-mode: soft-light;
+
+  /* blur 1 */
+  filter: blur(67.5px);
+
+
 }
 </style>
